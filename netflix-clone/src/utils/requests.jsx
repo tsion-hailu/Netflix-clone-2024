3575b846917f8a3afb1b1247f9da3a252f326764
tsion-hailu <hailutsion16@gmail.com>
2024-09-05 19:17:12 +0300

    requeat & axios

---
+++ netflix-clone/src/utils/requests.jsx
@@ -0,0 +1,13 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
+  fetchNetflixOrginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-us`,
+  fecthActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fecthComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fecthHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fecthRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fecthDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fecthTvShow: `/tv/popular?api_key=${API_KEY}&language=en-uspage=1`
+};
+export default requests;
